4953baea2cf5f76b6b0540a6047111e3955d5a8c
Rabik Thapa <rabikthapa00@gmail.com>
2023-12-05 18:07:29 +0545

    Second Commit

--- Bloggie.Web/obj/Debug/net8.0/Bloggie.Web.AssemblyInfo.cs
+++ Bloggie.Web/obj/Debug/net8.0/Bloggie.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bloggie.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da9d69158921ae76a0d94e5825824fefe870927e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bloggie.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bloggie.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
