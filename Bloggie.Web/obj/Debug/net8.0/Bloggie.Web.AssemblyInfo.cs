9285a25eead54702c21beabcf852d2d9f0b0655b
Rabik Thapa <rabikthapa00@gmail.com>
2023-12-08 18:24:53 +0545

    Saving BlogPost Entity with Tags to the Database

--- Bloggie.Web/obj/Debug/net8.0/Bloggie.Web.AssemblyInfo.cs
+++ Bloggie.Web/obj/Debug/net8.0/Bloggie.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bloggie.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f939714e2510562ab4c2ca273dbd437a96a3d33d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42833863f74666fc1a1d7f65c2f7f317e8208545")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bloggie.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bloggie.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
